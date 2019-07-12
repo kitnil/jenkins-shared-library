20782de271726776195b78eb7a7dd3f65e6f110d
Oleg Pykhalov <go.wigust@gmail.com>
2019-07-12 00:47:31 +0000

    Update guix.groovy

--- vars/guix.groovy
+++ vars/guix.groovy
@@ -1,7 +1,7 @@
 def build(String pack) {
-    sh "guix package --show=${pack}"
+    sh "./guix-jenkins/bin/guix package --show=${pack}"
     try {
-        sh "guix build --no-grafts ${pack}"
+        sh "./guix-jenkins/bin/guix build --no-grafts ${pack}"
     } catch (Exception error) {
         echo "Stage failed, but we still continue"
         slackSend color: '#1E90FF', message: "${pack} build failed"
