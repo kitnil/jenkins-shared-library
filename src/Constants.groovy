1054fbffa5eb802265a3e820759b0797e42bba31
Oleg Pykhalov <go.wigust@gmail.com>
2020-02-09 00:45:16 +0300

    src: Constants: Add gitGuixUrl.

--- src/Constants.groovy
+++ src/Constants.groovy
@@ -2,4 +2,5 @@ public class Constants {
     def main (_) {}
     public static final String gitUrl = "https://cgit.duckdns.org"
     public static final String homeDir = "/home/oleg"
+    public static final String gitGuixUrl = "https://cgit.duckdns.org/git/guix/guix"
 }
