2d04f4236d2049d8db9612631f209bbfbfe5f0b5
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-12 21:53:25 +0300

    vars: Add comGithub.
    * vars/comGithub.groovy: New file.
    * vars/gitRemote.groovy: New file.
    * src/Constants.groovy: Add githubOrganization.

--- src/Constants.groovy
+++ src/Constants.groovy
@@ -3,4 +3,5 @@ public class Constants {
     public static final String gitUrl = "https://cgit.duckdns.org"
     public static final String homeDir = "/home/oleg"
     public static final String gitGuixUrl = "https://cgit.duckdns.org/git/guix/guix"
+    public static final String githubOrganization = "kitnil"
 }
