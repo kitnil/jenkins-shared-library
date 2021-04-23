29d74d66077845f92645e37553de162671a54973
Oleg Pykhalov <go.wigust@gmail.com>
2021-04-23 20:18:32 +0300

    src: Constants: Update gitDotfilesUrl.

--- src/Constants.groovy
+++ src/Constants.groovy
@@ -3,7 +3,7 @@ public class Constants {
     public static final String gitUrl = "https://cgit.duckdns.org"
     public static final String homeDir = "/home/oleg"
     public static final String gitGuixUrl = "https://cgit.duckdns.org/git/guix/guix"
-    public static final String gitDotfilesUrl = "https://cgit.duckdns.org/git/wigust/dotfiles"
+    public static final String gitDotfilesUrl = "https://github.com/kitnil/dotfiles"
     public static final String githubOrganization = "kitnil"
     public static final String masterLabel = "guixsd"
 }
